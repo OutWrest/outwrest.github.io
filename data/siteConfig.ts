9b14d5a1b8c519d2db3e9ea76ca88a838a0e26c6
Salah <0utwrest@protonmail.com>
2024-07-13 21:46:13 -0700

    Updated site config

--- data/siteConfig.ts
+++ data/siteConfig.ts
@@ -1,13 +1,11 @@
 import type { SiteConfig } from "@/lib/types";
-const prefix = process.env.BASE_PATH || "";
-export { prefix };
 
 const siteConfig: SiteConfig = {
-  avatar: `${prefix}/avatar.png`,
+  avatar: `/avatar.png`,
   siteUrl: "https://outw.rest",
   siteName: "Outwrest",
   siteDescription: "Personal blog & whatever shananigans I'm up to.",
-  siteThumbnail: `${prefix}/og-image.png`,
+  siteThumbnail: `/og-image.png`,
   nav: [
     { label: "Posts", href: "/posts" },
     { label: "About", href: "/about" },
